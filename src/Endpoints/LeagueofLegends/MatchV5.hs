6bd6b81ddd4cc3198992116ab2f3481dae824003
Lukas Mahr <lukas@yousuckatprogramming.de>
2025-03-16 18:14:33 +0100

    Add query parameter to matchv5 get matchids by puuid

--- src/Endpoints/LeagueofLegends/MatchV5.hs
+++ src/Endpoints/LeagueofLegends/MatchV5.hs
@@ -4,19 +4,21 @@
 
 module Endpoints.LeagueofLegends.MatchV5 where
 
-import           Data.Aeson          (FromJSON (..), defaultOptions,
-                                      fieldLabelModifier, genericParseJSON)
-import           Data.Aeson.Types    (withObject, (.:), (.:?))
-import           Data.Map            (Map)
+import           Data.Aeson            (FromJSON (..), defaultOptions,
+                                        fieldLabelModifier, genericParseJSON)
+import           Data.Aeson.Types      (withObject, (.:), (.:?))
+import qualified Data.Bifunctor
+import qualified Data.ByteString.Char8 as BC
+import           Data.Map              (Map)
 import           GHC.Generics
-import           Network.HTTP.Client (parseRequest)
-import           PureRiot            (TypedRequest (TypedRequest))
-import           Regions             (Region (..), regionToUrl)
+import           Network.HTTP.Client   (parseRequest, setQueryString)
+import           PureRiot              (TypedRequest (TypedRequest))
+import           Regions               (Region (..), regionToUrl)
 
 data MatchDto = MatchDto
-  { {-Match metadata.-}
+  { {- | Match metadata.-}
     metadata :: MetadataDto,
-    {-Match info.-}
+    {- | Match info.-}
     info     :: InfoDto
   }
   deriving (Show, Eq, Generic)
@@ -24,11 +26,11 @@ data MatchDto = MatchDto
 instance FromJSON MatchDto
 
 data MetadataDto = MetadataDto
-  { {-Match data version.-}
+  { {- | Match data version.-}
     dataVersion  :: String,
-    {-Match id.-}
+    {- | Match id.-}
     matchId      :: String,
-    {-A list of participant PUUIDs.-}
+    {- | A list of participant PUUIDs.-}
     participants :: [String]
   }
   deriving (Show, Eq, Generic)
@@ -36,32 +38,32 @@ data MetadataDto = MetadataDto
 instance FromJSON MetadataDto
 
 data InfoDto = InfoDto
-  { {-Refer to indicate if the game ended in termination.-}
+  { {- | Refer to indicate if the game ended in termination.-}
     endOfGameResult    :: String,
-    {-Unix timestamp for when the game is created on the game server (i.e., the loading screen).-}
+    {- | Unix timestamp for when the game is created on the game server (i.e., the loading screen).-}
     gameCreation       :: Int,
-    {-Prior to patch 11.20, this field returns the game length in milliseconds calculated from gameEndTimestamp - gameStartTimestamp. Post patch 11.20, this field returns the max timePlayed of any participant in the game in seconds, which makes the behavior of this field consistent with that of match-v4. The best way to handling the change in this field is to treat the value as milliseconds if the gameEndTimestamp field isn't in the response and to treat the value as seconds if gameEndTimestamp is in the response.-}
+    {- | Prior to patch 11.20, this field returns the game length in milliseconds calculated from gameEndTimestamp - gameStartTimestamp. Post patch 11.20, this field returns the max timePlayed of any participant in the game in seconds, which makes the behavior of this field consistent with that of match-v4. The best way to handling the change in this field is to treat the value as milliseconds if the gameEndTimestamp field isn't in the response and to treat the value as seconds if gameEndTimestamp is in the response.-}
     gameDuration       :: Int,
-    {-Unix timestamp for when match ends on the game server. This timestamp can occasionally be significantly longer than when the match "ends". The most reliable way of determining the timestamp for the end of the match would be to add the max time played of any participant to the gameStartTimestamp. This field was added to match-v5 in patch 11.20 on Oct 5th, 2021.-}
+    {- | Unix timestamp for when match ends on the game server. This timestamp can occasionally be significantly longer than when the match "ends". The most reliable way of determining the timestamp for the end of the match would be to add the max time played of any participant to the gameStartTimestamp. This field was added to match-v5 in patch 11.20 on Oct 5th, 2021.-}
     gameEndTimestamp   :: Int,
     gameId             :: Int,
-    {-Refer to the Game Constants documentation.-}
+    {- | Refer to the Game Constants documentation.-}
     gameMode           :: String,
     gameName           :: String,
-    {-Unix timestamp for when match starts on the game server.-}
+    {- | Unix timestamp for when match starts on the game server.-}
     gameStartTimestamp :: Int,
     gameType           :: String,
-    {-The first two parts can be used to determine the patch a game was played on.-}
+    {- | The first two parts can be used to determine the patch a game was played on.-}
     gameVersion        :: String,
-    {-Refer to the Game Constants documentation.-}
+    {- | Refer to the Game Constants documentation.-}
     mapId              :: Int,
     participants       :: [ParticipantDto],
-    {-Platform where the match was played.-}
+    {- | Platform where the match was played.-}
     platformId         :: String,
-    {-Refer to the Game Constants documentation.-}
+    {- | Refer to the Game Constants documentation.-}
     queueId            :: Int,
     teams              :: [TeamDto],
-    {-Tournament code used to generate the match. This field was added to match-v5 in patch 11.13 on June 23rd, 2021.-}
+    {- | Tournament code used to generate the match. This field was added to match-v5 in patch 11.13 on June 23rd, 2021.-}
     tournamentCode     :: String
   }
   deriving (Show, Eq, Generic)
@@ -69,21 +71,21 @@ data InfoDto = InfoDto
 instance FromJSON InfoDto
 
 data ParticipantDto = ParticipantDto
-  { {-Yellow crossed swords-}
+  { {- | Yellow crossed swords-}
     allInPings                     :: Int,
-    {-Green flag-}
+    {- | Green flag-}
     assistMePings                  :: Int,
     assists                        :: Int,
     baronKills                     :: Int,
     bountyLevel                    :: Int,
     champExperience                :: Int,
     champLevel                     :: Int,
-    {-Prior to patch 11.4, on Feb 18th, 2021, this field returned invalid championIds. We recommend determining the champion based on the championName field for matches played prior to patch 11.4.-}
+    {- | Prior to patch 11.4, on Feb 18th, 2021, this field returned invalid championIds. We recommend determining the champion based on the championName field for matches played prior to patch 11.4.-}
     championId                     :: Int,
     championName                   :: String,
-    {-Blue generic ping (ALT+click)-}
+    {- | Blue generic ping (ALT+click)-}
     commandPings                   :: Int,
-    {-This field is currently only utilized for Kayn's transformations. (Legal values: 0 - None, 1 - Slayer, 2 - Assassin)-}
+    {- | This field is currently only utilized for Kayn's transformations. (Legal values: 0 - None, 1 - Slayer, 2 - Assassin)-}
     championTransform              :: Int,
     consumablesPurchased           :: Int,
     challenges                     :: ChallengesDto,
@@ -96,23 +98,23 @@ data ParticipantDto = ParticipantDto
     doubleKills                    :: Int,
     dragonKills                    :: Int,
     eligibleForProgression         :: Bool,
-    {-Yellow questionmark-}
+    {- | Yellow questionmark-}
     enemyMissingPings              :: Int,
-    {-Red eyeball-}
+    {- | Red eyeball-}
     enemyVisionPings               :: Int,
     firstBloodAssist               :: Bool,
     firstBloodKill                 :: Bool,
     firstTowerAssist               :: Bool,
     firstTowerKill                 :: Bool,
-    {-This is an offshoot of the OneStone challenge. The code checks if a spell with the same instance ID does the final point of damage to at least 2 Champions. It doesn't matter if they're enemies, but you cannot hurt your friends.-}
+    {- | This is an offshoot of the OneStone challenge. The code checks if a spell with the same instance ID does the final point of damage to at least 2 Champions. It doesn't matter if they're enemies, but you cannot hurt your friends.-}
     gameEndedInEarlySurrender      :: Bool,
     gameEndedInSurrender           :: Bool,
     holdPings                      :: Int,
-    {-Yellow circle with horizontal line-}
+    {- | Yellow circle with horizontal line-}
     getBackPings                   :: Int,
     goldEarned                     :: Int,
     goldSpent                      :: Int,
-    {-Both individualPosition and teamPosition are computed by the game server and are different versions of the most likely position played by a player. The individualPosition is the best guess for which position the player actually played in isolation of anything else. The teamPosition is the best guess for which position the player actually played if we add the constraint that each team must have one top player, one jungle, one middle, etc. Generally the recommendation is to use the teamPosition field over the individualPosition field.-}
+    {- | Both individualPosition and teamPosition are computed by the game server and are different versions of the most likely position played by a player. The individualPosition is the best guess for which position the player actually played in isolation of anything else. The teamPosition is the best guess for which position the player actually played if we add the constraint that each team must have one top player, one jungle, one middle, etc. Generally the recommendation is to use the teamPosition field over the individualPosition field.-}
     individualPosition             :: String,
     inhibitorKills                 :: Int,
     inhibitorTakedowns             :: Int,
@@ -136,16 +138,16 @@ data ParticipantDto = ParticipantDto
     magicDamageDealtToChampions    :: Int,
     magicDamageTaken               :: Int,
     missions                       :: MissionsDto,
-    {-neutralMinionsKilled = mNeutralMinionsKilled, which is incremented on kills of kPet and kJungleMonster-}
+    {- | neutralMinionsKilled = mNeutralMinionsKilled, which is incremented on kills of kPet and kJungleMonster-}
     neutralMinionsKilled           :: Int,
-    {-Green ward-}
+    {- | Green ward-}
     needVisionPings                :: Int,
     nexusKills                     :: Int,
     nexusTakedowns                 :: Int,
     nexusLost                      :: Int,
     objectivesStolen               :: Int,
     objectivesStolenAssists        :: Int,
-    {-Blue arrow pointing at ground-}
+    {- | Blue arrow pointing at ground-}
     onMyWayPings                   :: Int,
     participantId                  :: Int,
     playerScore0                   :: Maybe Int,
@@ -171,7 +173,7 @@ data ParticipantDto = ParticipantDto
     playerAugment3                 :: Int,
     playerAugment4                 :: Int,
     playerSubteamId                :: Int,
-    {-Green minion-}
+    {- | Green minion-}
     pushPings                      :: Int,
     profileIcon                    :: Int,
     puuid                          :: String,
@@ -194,7 +196,7 @@ data ParticipantDto = ParticipantDto
     summonerName                   :: String,
     teamEarlySurrendered           :: Bool,
     teamId                         :: Int,
-    {-Both individualPosition and teamPosition are computed by the game server and are different versions of the most likely position played by a player. The individualPosition is the best guess for which position the player actually played in isolation of anything else. The teamPosition is the best guess for which position the player actually played if we add the constraint that each team must have one top player, one jungle, one middle, etc. Generally the recommendation is to use the teamPosition field over the individualPosition field.-}
+    {- | Both individualPosition and teamPosition are computed by the game server and are different versions of the most likely position played by a player. The individualPosition is the best guess for which position the player actually played in isolation of anything else. The teamPosition is the best guess for which position the player actually played if we add the constraint that each team must have one top player, one jungle, one middle, etc. Generally the recommendation is to use the teamPosition field over the individualPosition field.-}
     teamPosition                   :: String,
     timeCCingOthers                :: Int,
     timePlayed                     :: Int,
@@ -204,11 +206,11 @@ data ParticipantDto = ParticipantDto
     totalDamageShieldedOnTeammates :: Int,
     totalDamageTaken               :: Int,
     totalEnemyJungleMinionsKilled  :: Int,
-    {-Whenever positive health is applied (which translates to all heals in the game but not things like regeneration), totalHeal is incremented by the amount of health received. This includes healing enemies, jungle monsters, yourself, etc-}
+    {- | Whenever positive health is applied (which translates to all heals in the game but not things like regeneration), totalHeal is incremented by the amount of health received. This includes healing enemies, jungle monsters, yourself, etc-}
     totalHeal                      :: Int,
-    {-Whenever positive health is applied (which translates to all heals in the game but not things like regeneration), totalHealsOnTeammates is incremented by the amount of health received.  This is post modified, so if you heal someone missing 5 health for 100 you will get +5 totalHealsOnTeammates-}
+    {- | Whenever positive health is applied (which translates to all heals in the game but not things like regeneration), totalHealsOnTeammates is incremented by the amount of health received.  This is post modified, so if you heal someone missing 5 health for 100 you will get +5 totalHealsOnTeammates-}
     totalHealsOnTeammates          :: Int,
-    {-totalMillionsKilled = mMinionsKilled, which is only incremented on kills of kTeamMinion, kMeleeLaneMinion, kSuperLaneMinion, kRangedLaneMinion and kSiegeLaneMinion-}
+    {- | totalMillionsKilled = mMinionsKilled, which is only incremented on kills of kTeamMinion, kMeleeLaneMinion, kSuperLaneMinion, kRangedLaneMinion and kSiegeLaneMinion-}
     totalMinionsKilled             :: Int,
     totalTimeCCDealt               :: Int,
     totalTimeSpentDead             :: Int,
@@ -321,7 +323,7 @@ data ChallengesDto = ChallengesDto
     maxKillDeficit                            :: Maybe Int,
     mejaisFullStackInTime                     :: Maybe Int,
     moreEnemyJungleThanOpponent               :: Maybe Double,
-    {-This is an offshoot of the OneStone challenge. The code checks if a spell with the same instance ID does the final point of damage to at least 2 Champions. It doesn't matter if they're enemies, but you cannot hurt your friends.-}
+    {- | This is an offshoot of the OneStone challenge. The code checks if a spell with the same instance ID does the final point of damage to at least 2 Champions. It doesn't matter if they're enemies, but you cannot hurt your friends.-}
     multiKillOneSpell                         :: Maybe Int,
     multikills                                :: Maybe Int,
     multikillsAfterAggressiveFlash            :: Maybe Int,
@@ -371,7 +373,7 @@ data ChallengesDto = ChallengesDto
     teamRiftHeraldKills                       :: Maybe Int,
     tookLargeDamageSurvived                   :: Maybe Int,
     turretPlatesTaken                         :: Maybe Int,
-    {-Any player who damages a tower that is destroyed within 30 seconds of a Rift Herald charge will receive credit. A player who does not damage the tower will not receive credit .-}
+    {- | Any player who damages a tower that is destroyed within 30 seconds of a Rift Herald charge will receive credit. A player who does not damage the tower will not receive credit .-}
     turretsTakenWithRiftHerald                :: Maybe Int,
     turretTakedowns                           :: Maybe Int,
     twentyMinionsIn3SecondsCount              :: Maybe Int,
@@ -496,9 +498,9 @@ data ObjectiveDto = ObjectiveDto
 instance FromJSON ObjectiveDto
 
 data TimelineDto = TimelineDto
-  { {-Match metadata.-}
+  { {- | Match metadata.-}
     metadata :: MetadataTimeLineDto,
-    {-Match info.-}
+    {- | Match info.-}
     info     :: InfoTimeLineDto
   }
   deriving (Show, Eq, Generic)
@@ -506,11 +508,11 @@ data TimelineDto = TimelineDto
 instance FromJSON TimelineDto
 
 data MetadataTimeLineDto = MetadataTimeLineDto
-  { {-Match data version.-}
+  { {- | Match data version.-}
     dataVersion  :: String,
-    {-Match id.-}
+    {- | Match id.-}
     matchId      :: String,
-    {-A list of participant PUUIDs.-}
+    {- | A list of participant PUUIDs.-}
     participants :: [String]
   }
   deriving (Show, Eq, Generic)
@@ -518,7 +520,7 @@ data MetadataTimeLineDto = MetadataTimeLineDto
 instance FromJSON MetadataTimeLineDto
 
 data InfoTimeLineDto = InfoTimeLineDto
-  { {-Refer to indicate if the game ended in termination.-}
+  { {- | Refer to indicate if the game ended in termination.-}
     endOfGameResult :: String,
     frameInterval   :: Int,
     gameId          :: Int,
@@ -648,10 +650,40 @@ newtype Puuids = Puuids [String] deriving (Show, Eq, Generic)
 
 instance FromJSON Puuids
 
-matchesByPuuidId :: Region -> Puuid -> IO (TypedRequest Puuids)
-matchesByPuuidId region puuid = do
-  request <- parseRequest $ regionToUrl region ++ "/lol/match/v5/matches/by-puuid/" ++ puuid ++ "/ids"
-  return $ TypedRequest request
+{- | Epoch timestamp in seconds. The matchlist started storing timestamps on June 16th, 2021. Any matches played before June 16th, 2021 won't be included in the results if the startTime filter is set.-}
+type StartTime = Integer
+
+-- | Epoch timestamp in seconds.
+type EndTime = Integer
+
+-- |Filter the list of match ids by a specific queue id. This filter is mutually inclusive of the type filter meaning any match ids returned must match both the queue and type filters.
+type Queue = Int
+
+-- | Filter the list of match ids by the type of match. This filter is mutually inclusive of the queue filter meaning any match ids returned must match both the queue and type filters.
+type Type = String
+
+-- | Defaults to 0. Start index.
+type Start = Int
+
+-- | Defaults to 20. Valid values: 0 to 100. Number of match ids to return.
+type Count = Int
+
+packQueryParams :: [(String, String)] -> [(BC.ByteString, Maybe BC.ByteString)]
+packQueryParams = map (Data.Bifunctor.bimap BC.pack (Just . BC.pack))
+
+matchesByPuuidId :: Region -> Puuid -> StartTime -> EndTime -> Queue -> Type -> Start -> Count -> IO (TypedRequest Puuids)
+matchesByPuuidId region puuid startTime endTime queue qtype start count = do
+  let baseUrl = regionToUrl region ++ "/lol/match/v5/matches/by-puuid/" ++ puuid ++ "/ids"
+  request <- parseRequest baseUrl
+  let params = packQueryParams
+          [ ("startTime", show startTime),
+            ("endTime", show endTime),
+            ("queue", show queue),
+            ("type", show qtype),
+            ("start", show start),
+            ("count", show count)
+          ]
+  return $ TypedRequest (setQueryString params request)
 
 matches :: Region -> MatchId -> IO (TypedRequest MatchDto)
 matches region matchId = do
